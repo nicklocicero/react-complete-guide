9b78f9104241141a4b99df4be6bc69aee6b6d1fe
nicklocicero <nicklocicero@gmail.com>
2019-01-07 13:15:44 -0700

    added two way binding for components.

--- src/App.js
+++ src/App.js
@@ -12,12 +12,22 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked');
     this.setState({
       persons: [
-        { name: 'Jake', age: 31 },
+        { name: newName, age: 31 },
         { name: 'Jiles', age: 29 },
+        { name: 'Jill', age: 50 }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Gilly', age: 31 },
+        { name: event.target.value, age: 29 },
         { name: 'Jill', age: 50 }
       ]
     });
@@ -28,10 +38,18 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Steele built railroad')} 
+          changed={this.nameChangedHandler}>My hobbies: racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\' a React app!!!'));
