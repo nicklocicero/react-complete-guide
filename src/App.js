402738958b53b404357a9c6c52d7d5087d298dd8
nicklocicero <nicklocicero@gmail.com>
2019-01-13 16:29:32 -0700

    added an ErrorBoundary component to practice catching errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person.js'
+import Person from './Person/Person.js';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js'
 
 class App extends Component {
   // state updates the dom when changed, re-renders page
@@ -53,12 +54,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age} 
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)} />
+              <ErrorBoundary
+                key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age} 
+                  changed={(event) => this.nameChangedHandler(event, person.id)} />
+              </ErrorBoundary>
             )
           })}
         </div>
