a38b752d54d7267e5f0dd1a559d20c09d0cfa1e6
nicklocicero <nicklocicero@gmail.com>
2019-01-13 20:02:44 -0700

    Revert "added an ErrorBoundary component to practice catching errors"
    This reverts commit 402738958b53b404357a9c6c52d7d5087d298dd8.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person.js';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js'
+import Person from './Person/Person.js'
 
 class App extends Component {
   // state updates the dom when changed, re-renders page
@@ -54,14 +53,12 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary
-                key={person.id}>
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age} 
-                  changed={(event) => this.nameChangedHandler(event, person.id)} />
-              </ErrorBoundary>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age} 
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)} />
             )
           })}
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
 import classes from './Person.css'
+
 const person = (props) => {
     const style = {
         '@media (min-width: 500px)': {
             width: '450px'
         }
     }
-    const rnd = Math.random();
-
-
-    if (rnd > 0.8) {
-        throw new Error( 'Something went wrong' );
-    }
-
-
     return (
         <div className={classes.Person} style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
